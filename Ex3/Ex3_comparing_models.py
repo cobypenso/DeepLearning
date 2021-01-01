0edf26ca4799254db0c7f1b4eecd5b1aa4ec1420
cobypenso <coby.penso24@gmail.com>
2021-01-01 13:28:53 +0200

    dev

--- Ex3/Ex3_comparing_models.py
+++ Ex3/Ex3_comparing_models.py
@@ -1,5 +1,6 @@
 # Coby Penso, 208254128
 import numpy as np
+from numpy.core.fromnumeric import size
 import pandas as pd
 import matplotlib.pyplot as plt
 from utils import *
@@ -9,13 +10,35 @@ from torchvision import transforms
 import torchvision
 from models import *
 
-def train(net, trainloader, optimizer, criterion ,numEpochs):
+use_cuda = torch.cuda.is_available()
+seed = 42
+# Set seed
+np.random.seed(seed)
+torch.manual_seed(seed)
+if use_cuda:
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+# Handel GPU stochasticity
+torch.backends.cudnn.enabled = use_cuda
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+
+device = torch.device("cuda" if use_cuda else "cpu")
+
+def train(net, trainloader, validloader, optimizer, criterion ,numEpochs):
+    train_loss = 0
+
+    net = net.to(device)
+
     for epoch in range(numEpochs):  # loop over the dataset multiple times
+        net.train()
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
 
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
 
@@ -27,16 +50,21 @@ def train(net, trainloader, optimizer, criterion ,numEpochs):
 
             # print statistics
             running_loss += loss.item()
+            train_loss = running_loss
             if i % 10 == 0:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 10))
                 running_loss = 0.0
+            # test on validation dataset
+        valid_acc = test(net, validloader)
 
     print('Finished Training')
+    return train_loss, valid_acc
 
 def test(net, testloader):
     correct = 0
     total = 0
+    net.eval()
     with torch.no_grad():
         for data in testloader:
             images, labels = data
@@ -44,9 +72,58 @@ def test(net, testloader):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+    net.train()
+    return (100 * correct / total)
 
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
+def get_loaders(train_transform, test_transform, batch_size):
+    trainset = torchvision.datasets.STL10(root='./data', split='train',
+                                            download=True, transform=train_transform)
+    trainset, validset = split_train_and_val(trainset, 0.8)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=1)
+    validloader = torch.utils.data.DataLoader(validset, batch_size=batch_size,
+                                            shuffle=True, num_workers=1)
+    testset = torchvision.datasets.STL10(root='./data', split='test',
+                                        download=True, transform=test_transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=1)
+    
+    return trainloader, validloader, testloader
+
+def get_optimizer(model_params, optimizer_type, lr, weight_decay):
+
+    if optimizer_type == 'SGD':
+        optimizer = optim.SGD(model_params, lr=lr, momentum=0.9, weight_decay=weight_decay)
+    elif optimizer_type == 'Adam':
+        optimizer = optim.Adam(model_params, lr=lr, betas=(0.9, 0.999), weight_decay=weight_decay)
+    elif optimizer_type == 'RMSProp':
+        optimizer = optim.Adam(model_params, lr=lr, alpha=0.99, eps=1e-08, weight_decay=weight_decay)
+    else:
+        NotImplementedError("optimizer not implemented")
+    return optimizer
+
+def hyperparam_search(net, net_params_to_learn, criterion, train_transform, test_transform):
+    
+    numEpochs = 60
+
+    results = []
+    for lr in [0.01, 0.001]:
+        for bt in [16, 64, 128]:
+            for wd in [0.01, 0.001]:
+                for opt_type in ['SGD', 'Adam', 'RMSProp']:
+                    optimizer = get_optimizer(net_params_to_learn, opt_type, lr, weight_decay=wd)
+                    trainloader, validloader, testloader = get_loaders(train_transform, test_transform, bt)
+                    
+                    train_loss, valid_accuracy = train(net, trainloader, validloader, optimizer, criterion, numEpochs)
+                    test_accuracy = test(net, testloader)
+
+                    results.append({'lr': lr,
+                                    'wd': wd,
+                                    'bt': bt,
+                                    'optimizer_type': opt_type,
+                                    'loss': train_loss,
+                                    'test_accuracy': test_accuracy})
+
 
 ###########################################
 ################### Main ##################
@@ -55,35 +132,37 @@ def test(net, testloader):
 def main(args):
     mean, std = (0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
+    train_transform = transforms.Compose(
+        [transforms.Resize(48),
+        transforms.RandomCrop(32),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)]) 
+
+    test_transform = transforms.Compose(
+        [transforms.Resize(48),
+        transforms.CenterCrop(32),
+        transforms.ToTensor(),
         transforms.Normalize(mean, std)])
-
-    trainset = torchvision.datasets.STL10(root='./data', split='train',
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
-                                            shuffle=True, num_workers=0)
-
-    testset = torchvision.datasets.STL10(root='./data', split='test',
-                                        download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=32,
-                                            shuffle=False, num_workers=0)
-
-    # classes = ('plane', 'car', 'bird', 'cat',
-    #         'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    
+    if args.visualize:
+        trainset = torchvision.datasets.STL10(root='./data', split='train',
+                                            download=True, transform=train_transform)
+        visualize(trainset, 4)
 
     numEpochs = 20
     hidden_sizes = [1000, 500, 100, 10]
     num_classes = 10
-    image_dim = 96 * 96 * 3
+    image_dim = 32 * 32 * 3
     criterion = nn.CrossEntropyLoss()
     
     if args.net == 'LR':
         #Logistic Regression net
         logistic_regression_net = LogisticRegression(image_dim, num_classes)
-        optimizer = optim.SGD(logistic_regression_net.parameters(), lr=0.001, momentum=0.9)
-        train(logistic_regression_net, trainloader, optimizer, criterion, numEpochs)
-        test(logistic_regression_net, testloader)
+        hyperparam_search(logistic_regression_net,
+                          logistic_regression_net.parameters(), 
+                          criterion,
+                          train_transform, test_transform)
     elif args.net == 'FC3':
         #FullyConnected 3Hidden Layers+Dropout+BN
         fc_net = FC3_Net(image_dim, num_classes)
@@ -115,6 +194,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Neural Network")
-    parser.add_argument("--net", default="ResNet18_feature_extractor", type=str, choices=['LR', 'FC3', 'CNN','ResNet18_fine_tune', 'ResNet18_feature_extractor'])
+    parser.add_argument("--net", default="LR", type=str, choices=['LR', 'FC3', 'CNN','ResNet18_fine_tune', 'ResNet18_feature_extractor'])
+    parser.add_argument("--optimizer_type", default="SGD", type=str, choices=['SGD', 'Adam', 'RMSProp'])
+    parser.add_argument("--visualize", default=False, type=bool)
     args = parser.parse_args()
     main(args)
--- Ex3/utils.py
+++ Ex3/utils.py
@@ -2,55 +2,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import torch
 
-from numpy.testing._private.utils import break_cycles
-
-def softmax(z):
-    """implement the softmax functions
-    @params: z - array to apply softmax on
-    @returns: result - softmax
-    """
-    max_logist = np.max(z, axis = 1, keepdims=True)
-    normalized_z = z - max_logist
-    exp_list = np.exp(normalized_z)
-    result = exp_list * (1 / np.sum(exp_list, axis=1, keepdims=True))
-    result = result.reshape((len(z),-1))
-
-    return result
-
-def neg_log_loss(pred, label):
-    """implement the negative log loss
-    @params: 
-        pred
-        label
-    @returns: loss
-    """
-    log_likelihood = -np.log(pred[range(pred.shape[0]),label])
-    loss = np.sum(log_likelihood) / pred.shape[0]
-    return loss
-
-def split_train_and_val(X, Y, precentage = 0.8):
+def split_train_and_val(dataset, train_percentage):
     '''
     @params:
-        X  - images
-        Y  - labels
-        precentage - precentage to be training and the rest would be validation
-    @returns: train_training, train_labels, validation_labels, validation_labels
+        dataset - torch dataset to split to train and validation
+        train_percentage - the percentile of the dataset to be for train and the rest for validation
+
+    @returns:
+        (train_dataset, validation_dataset)
     '''
-    partition = int(X.shape[0] * precentage)
-
-    s = np.arange(X.shape[0])
-    s = np.random.shuffle(s)
-    X_shuffle = (X[s])[0]
-    Y_shuffle = (Y[s])[0]
-
-    return np.array(X_shuffle[:partition]), \
-           np.array(Y_shuffle[:partition]), \
-           np.array(X_shuffle[partition:]), \
-           np.array(Y_shuffle[partition:])
+    train_size = int(train_percentage * len(dataset))
+    test_size = len(dataset) - train_size
+    return torch.utils.data.random_split(dataset, [train_size, test_size])
 
 
-def visualize(data, labels, pics_from_each_labels = 4):
+def visualize(dataset, pics_from_each_labels = 4):
     '''
     visualize the dataset
     @params:
@@ -60,32 +28,17 @@ def visualize(data, labels, pics_from_each_labels = 4):
     '''
     pics = {}
     for j in range(10):
-        pics[j] = [data[i] for i in range(len(data)) if (labels[i] == j)][0:4]
+        idx = dataset.labels==j
+        pics[j] = (dataset.data[idx])[0:pics_from_each_labels]
 
     fig, axs = plt.subplots(nrows=pics_from_each_labels, ncols = 10, figsize=(20,20)) # specifying the overall grid size
     for j in range(pics_from_each_labels):
         for i in range(10):
-            axs[j, i].imshow(pics[i][j].reshape(28,28), cmap='gray')
+            axs[j, i].axis("off")
+            axs[j, i].imshow(np.transpose(pics[i][j], (1,2,0)))
             axs[j, i].set_title("Class {}-Image {}".format(i, j))
     plt.tight_layout()
     plt.show()
-
-def normalize(data):
-    '''
-    normalize the data 
-    @params:
-        data - data to be normalized
-    '''
-    # get min and max per feature (except the first one which corresponds to the bias term)
-    X_min = np.min(data[:, 1:], axis=0)
-    X_max = np.max(data[:, 1:], axis=0)
-    return (data[:, 1:] - X_min)/(X_max - X_min + 1e-8)
-
-def ReLU(x):
-    '''
-    non linearity
-    '''
-    return np.maximum(0, x)
 
 def save_model(model, file):
     '''
