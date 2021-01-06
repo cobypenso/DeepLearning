0942725c1c20dcc03334959605c6103c5ce42f09
cobypenso <coby.penso24@gmail.com>
2021-01-06 19:05:56 +0200

    added loss track and ploting

--- Ex3/Ex3_comparing_models.py
+++ Ex3/Ex3_comparing_models.py
@@ -1,5 +1,6 @@
 # Coby Penso, 208254128
 import numpy as np
+from numpy.core import numeric
 from numpy.core.fromnumeric import size
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -27,6 +28,8 @@ torch.backends.cudnn.deterministic = True
 device = torch.device("cuda" if use_cuda else "cpu")
 
 def train(net, trainloader, validloader, optimizer, criterion ,numEpochs):
+    train_loss_list = []
+    valid_loss_list = []
     train_loss = 0
 
     net = net.to(device)
@@ -52,20 +55,26 @@ def train(net, trainloader, validloader, optimizer, criterion ,numEpochs):
             # print statistics
             running_loss += loss.item()
             # test on validation dataset
+
         train_loss = running_loss / i
-        valid_acc = test(net, validloader)
+        train_loss_list.append(train_loss)
+        valid_acc, valid_loss = test(net, validloader, criterion)
+        valid_loss_list.append(valid_loss)
+
         print ("epoch: " + str(epoch) + " valid accuracy: " + str(valid_acc) + " train loss: " + str(train_loss))
 
     print('Finished Training')
-    return train_loss, valid_acc
+    return train_loss_list, valid_loss_list
 
-def test(net, testloader):
+def test(net, testloader, criterion):
     net.eval()
     correct = 0
     total = 0
+    running_loss = 0.0
     net.eval()
+
     with torch.no_grad():
-        for data in testloader:
+        for i, data in enumerate(testloader, 0):
             images, labels = data
             images = images.to(device)
             labels = labels.to(device)
@@ -74,8 +83,14 @@ def test(net, testloader):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+
+            loss = criterion(outputs, labels)
+            running_loss += loss.item()
+
+        test_loss = running_loss / i
+
     net.train()
-    return (100 * correct / total)
+    return (100 * correct / total), test_loss
 
 def get_loaders(train_transform, test_transform, batch_size):
     trainset = torchvision.datasets.STL10(root='./data', split='train',
@@ -121,14 +136,14 @@ def hyperparam_search(net, criterion, train_transform, test_transform):
                     optimizer = get_optimizer(net, opt_type, lr, weight_decay=wd)
                     trainloader, validloader, testloader = get_loaders(train_transform, test_transform, bt)
                     
-                    train_loss, valid_accuracy = train(net, trainloader, validloader, optimizer, criterion, numEpochs)
-                    test_accuracy = test(net, testloader)
+                    train_loss, valid_loss = train(net, trainloader, validloader, optimizer, criterion, numEpochs)
+                    test_accuracy, _ = test(net, testloader, criterion)
                     print(test_accuracy)
                     results.append({'lr': lr,
                                     'wd': wd,
                                     'bt': bt,
                                     'optimizer_type': opt_type,
-                                    'loss': train_loss,
+                                    'loss': train_loss[-1],
                                     'test_accuracy': test_accuracy})
     return results
 
@@ -165,50 +180,72 @@ def main(args):
     
     if args.net == 'LR':
         #Logistic Regression net
-        logistic_regression_net = LogisticRegression(image_dim, num_classes, device)
-        results = hyperparam_search(logistic_regression_net,
-                          criterion,
-                          train_transform, test_transform)
-                          
+        net = LogisticRegression(image_dim, num_classes, device)
+        batch_size = 128
+        lr = 0.001
+        wd = 0
+        opt_type = 'SGD'             
     elif args.net == 'FC3':
         #FullyConnected 3Hidden Layers+Dropout+BN
-        fc_net = FC3_Net(image_dim, num_classes, device)
-        results = hyperparam_search(fc_net,
-                          criterion,
-                          train_transform, test_transform)
-        
+        net = FC3_Net(image_dim, num_classes, device)    
+        batch_size = 64
+        lr = 0.001
+        wd = 0
+        opt_type = 'Adam'            
     elif args.net == 'CNN':
         #CNN with 2 Conv layers
-        cnn_net = CNN_Net(image_dim, num_classes, device)
-        results = hyperparam_search(cnn_net,
-                          criterion,
-                          train_transform, test_transform)
-                          
+        net = CNN_Net(image_dim, num_classes, device)
+        batch_size = 32
+        lr = 0.01
+        wd = 0.01
+        opt_type = 'SGD'       
     elif args.net =='ResNet18_fine_tune':
         #ResNet18 Fine Tuning of all the paramters of the net
-        resnet18_fine_tuning = PreTrained_ResNet18(hidden_sizes, num_classes, False, device)
-        resnet18_fine_tuning.apply(init_weights)
-        results = hyperparam_search(resnet18_fine_tuning,
-                          criterion,
-                          train_transform, test_transform)
-
+        net = PreTrained_ResNet18(hidden_sizes, num_classes, False, device)
+        batch_size = 32
+        lr = 0.01
+        wd = 0
+        opt_type = 'Adam'  
     elif args.net == 'ResNet18_feature_extractor':
         #ResNet18 as only a feature extractor
-        resnet18_feature_extractor_only = PreTrained_ResNet18(hidden_sizes, num_classes, True, device)
-        resnet18_feature_extractor_only.apply(init_weights)
-        results = hyperparam_search(resnet18_feature_extractor_only,
-                          criterion,
-                          train_transform, test_transform)
+        net = PreTrained_ResNet18(hidden_sizes, num_classes, True, device)
+        batch_size = 32
+        lr = 0.01
+        wd = 0
+        opt_type = 'Adam'     
     else:
         raise NotImplementedError
 
-    df = pd.DataFrame(results)
-    df.to_csv(args.net + '60_epochs_summary.csv')
+    if args.hyper_parameter_search:        
+        results = hyperparam_search(net, criterion, train_transform, test_transform)
+        df = pd.DataFrame(results)
+        df.to_csv(args.net + '_hyper_param_summary.csv')
+    
+    if args.train_best_model:
+        numEpochs = 300
+        net.apply(init_weights)
+        optimizer = get_optimizer(net, opt_type, lr, weight_decay=wd)
+        trainloader, validloader, testloader = get_loaders(train_transform, test_transform, batch_size)
+        train_loss, valid_loss = train(net, trainloader, validloader, optimizer, criterion, numEpochs)
+        test_accuracy = test(net, testloader)
+
+        plt.figure()       
+        # Plot both train and test loss on the same figure
+        plt.plot(train_loss, linewidth=3, color='red', label='Train ELBO')
+        plt.plot(valid_loss, linewidth=3, color='green', label='Test ELBO')
+        plt.legend()
+        plt.xlabel('Epoch', fontsize=12)
+        plt.ylabel('Loss', fontsize=12)
+        plt.grid(True)
+        # Save the plot as a png image
+        plt.savefig('train&valid_loss_'+args.net+'.png')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Neural Network")
     parser.add_argument("--net", default="ResNet18_feature_extractor", type=str, choices=['LR', 'FC3', 'CNN','ResNet18_fine_tune', 'ResNet18_feature_extractor'])
     parser.add_argument("--optimizer_type", default="SGD", type=str, choices=['SGD', 'Adam', 'RMSProp'])
     parser.add_argument("--visualize", default=False, type=bool)
+    parser.add_argument("--train_best_model", default=True, type=bool)
+    parser.add_argument("--hyper_parameter_search", default=False, type=bool)
     args = parser.parse_args()
     main(args)
